ef35e5f34d2d3d37ee0249c9fc5863881395f040
Tim Wisbauer <Tim Wisbauer>
2020-09-21 11:23:49 -0500

    Added stop_ec2 code

--- ec2_schedule_tags/stop_ec2.py
+++ ec2_schedule_tags/stop_ec2.py
@@ -5,4 +5,21 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-    pass
+
+    ec2_client = boto3.client('ec2')
+
+    # Iterate over all regions.
+    for region in ec2_client.describe_regions()['Regions']:
+        logger.info(f"Region: {region}")
+        ec2_resource = boto3.resource('ec2', region_name = region)
+
+        # Get stopped and tagged instances.
+        instances = ec2_resource.instances.filter(
+            Filters=[{'Name': 'instance-state-name',
+                      'Values': ['running']}, 
+                      {'Name':'tag:lambda_scheduled', 'Values': 'true'}]
+        )
+
+        for instance in instances:
+            logger.info(f"Stopping instance: {instance.id}")
+            instance.stop()
